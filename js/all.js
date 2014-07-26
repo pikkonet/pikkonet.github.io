944749ac68d8fb24577e8b9a5dbdfafb2f1cf51b
eladamrani <eladamrani@gmail.com>
2014-07-26 23:58:19 +0300

    scroll to

--- js/all.js
+++ js/all.js
@@ -3,4 +3,18 @@ jQuery(document).ready(function() {
 
   $('body').fadeIn(2000);
  
+  $('.top_bar li.pos_2').click(function(){
+
+    $('html,body').animate({
+        scrollTop: $('#second-block').offset().top
+        }, 'slow');
+  });
+
+  $('.top_bar li.pos_0').click(function(){
+
+  $('html,body').animate({
+      scrollTop: $('#contact-block').offset().top
+      }, 'slow');
+  });
+
 });
